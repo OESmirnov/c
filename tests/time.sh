5193624d5a593873cda1bf74627f4cb186548bef
Oleg Smirnov <o.e.smirnov@yandex.ru>
2013-09-21 09:58:20 +0400

    Update time.sh

--- tests/time.sh
+++ tests/time.sh
@@ -4,9 +4,9 @@ BRUTE_PATH="./brute"
 
 echo "Write password:"
 read VAL
-HASH=$(htpasswd -nbd $VAL $VAL)
+HASH=$(htpasswd -nbd A $VAL)
 for MODE in "-i -s" "-r -s" "-i -m" "-r -m"
 do
 	echo -e '\n\n'"KEYS = " $MODE
-	time "${BRUTE_PATH}" ${MODE} "${HASH:5}"
+	time "${BRUTE_PATH}" ${MODE} "${HASH/A:/}"
 done
